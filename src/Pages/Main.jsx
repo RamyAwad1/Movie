56a03e11e0a9c8249d6090260360b8978bd12724
Ramy Awad <ramyawad685@gmail.com>
2025-05-07 17:21:33 +0300

    here we go

---
+++ src/Pages/Main.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import axios from "axios"
+
+
+
+function Home() {
+    const [movies, setMovies] = useState([]);
+
+
+
+    useEffect(() => {
+        axios("https://api.themoviedb.org/3/movie/popular?api_key=7e7e2b26b918ed77ee99eec0e3310101")
+            .then((moviesResponse) => {
+                setMovies(moviesResponse.data.results.slice(0, 10));
+            })
+    }, []);
+    return (
+
+        <>
+            <h1 style={{ textAlign: "center", padding: "15px" }}>Trending movies</h1>
+            <ul>
+                {movies.map((movie) => {
+                    return (
+                        <li key={movie.id}>
+                            <div>
+                                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                                    alt={movie.title} />
+                                <br />
+                                <p>{movie.title}</p>
+                            </div>
+                        </li>
+                    )
+                }
+                )}
+            </ul>
+
+        </>
+    )
+
+}
+export default Home
